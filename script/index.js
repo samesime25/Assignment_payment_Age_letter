058bc3b25f3cff47c95695e6b253fa790c6d52fa
samesime25 <samesime25@gmail.com>
2023-10-10 18:41:08 +0100

    Assignment on Payment, Age and Name

--- script/index.js
+++ script/index.js
@@ -1,24 +1,42 @@
 
    
-  // write using if statement to check if payment is made
+  /*Write  Javascript code using if statement to check if
+  Grade A
+   if payment is made,&&
+    if Age is above &&
+    if Name Contain A 
+    
+     Grade B
+   if payment is made,&&
+    if Age is above ||
+    if Name Contain A
+
+
+     Grade C
+   if payment is made,||
+    if Age is above &&
+    if Name Contain A
+    */
   let Payment = true;
   let Age = 17;
   let Name = "Edison";
 
   const verification_A = ()=>{
+    // Grade A
     if (Payment && Age > 16 && Name.includes("a")){
       console.log(`Payment was successful,  year age is above ${Age} and your ${Name} and also contain letter "A"`);
 
     }
+      // Grade B
     else if(Payment && Age > 16 || Name.includes("a")){
       console.log(`Payment was successful,  year age is above ${Age} and your ${Name} does not contain letter "A"`)
     }
-
+  // Grade C
     else if(Payment=== false || Age > 16 && Name.includes("a")){
       console.log(`Payment was  not successful,  year age is above ${Age} and your ${Name}  contain letter "A"`)
     }
 
-
+// default
  else{
   console.log(`Payment was not  successful , year age is less ${Age} and your ${Name} does not contain letter "A"`)
  }
@@ -26,7 +44,5 @@
   verification_A()
 
 
-
-// write  a code to check if their age is above  16 and 
 
 
